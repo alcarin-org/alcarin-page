67931bcbaaaf4c981345108184c808409fbe1f2c
Wiktor Obrębski <wiktor.obrebski@10clouds.com>
2015-03-06 09:09:40 +0100

    coffeescript and html minifaction, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,23 @@ var watch = require('gulp-watch');
 
 var wiredep = require('wiredep').stream;
 var connect = require('gulp-connect');
+var clean = require('gulp-clean');
+
+var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglifyjs');
+var ngAnnotate = require('gulp-ng-annotate');
 
 var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
 var concatCss = require('gulp-concat-css');
+
+var minifyHTML = require('gulp-minify-html');
+
+gulp.task('clean', function () {
+  gulp.src('./dist')
+    .pipe(clean());
+});
+
 
 gulp.task('connect', function () {
   connect.server({
@@ -14,6 +27,22 @@ gulp.task('connect', function () {
     livereload: true
   });
 });
+
+
+gulp.task('coffee', function() {
+  gulp.src('./src/**/*.coffee')
+    .pipe(coffee({bare: true}))
+    .pipe(ngAnnotate())
+    .pipe(uglify('alcarin.js'))
+    .pipe(gulp.dest('dist/static'))
+    .pipe(connect.reload());
+});
+gulp.task('watch-coffee', function () {
+  watch('src/**/*.coffee', function () {
+    gulp.start('coffee');
+  });
+});
+
 
 gulp.task('less', function () {
   gulp.src('src/alcarin/**/*.less')
@@ -26,12 +55,12 @@ gulp.task('less', function () {
     .pipe(gulp.dest('dist/static'))
     .pipe(connect.reload());
 });
-
 gulp.task('watch-less', function () {
   watch('src/**/*.less', function () {
     gulp.start('less');
   });
 });
+
 
 gulp.task('wiredep', function () {
   gulp.src('./src/index.html')
@@ -39,7 +68,8 @@ gulp.task('wiredep', function () {
       directory: 'lib/bower',
       ignorePath: '../lib/'
     }))
-    .pipe(gulp.dest('./src'));
+    .pipe(gulp.dest('./src'))
+    .pipe(connect.reload());
 });
 gulp.task('watch-bower', function () {
   watch('bower.json', function () {
@@ -47,9 +77,21 @@ gulp.task('watch-bower', function () {
   });
 });
 
+gulp.task('minify-html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./dist/static'))
+    .pipe(connect.reload());
+});
+gulp.task('watch-html', function() {
+  watch('src/**/*.html', function () {
+    gulp.start('minify-html');
+  });
+});
 
-gulp.task('build', ['less', 'wiredep']);
 
-gulp.task('watch', ['watch-less', 'watch-bower']);
+gulp.task('build', ['clean', 'coffee', 'less', 'wiredep', 'minify-html']);
+
+gulp.task('watch', ['watch-coffee', 'watch-less', 'watch-bower', 'watch-html']);
 gulp.task('serve', ['connect', 'watch']);
 gulp.task('default', ['serve']);
