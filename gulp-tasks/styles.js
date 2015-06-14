7fcecc1c9d435cc900e6cd527c0161572f173d0f
Wiktor Obrębski <psychowico@gmail.com>
2015-06-14 19:45:42 +0200

    slowly create more readable sass files

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -12,42 +12,32 @@ gulp.task('compile-styles', [
 /////
 
 function watchSassFiles() {
-  plugins.watch('src/assets/style/**/*.scss', function () {
-    gulp.start('compile-styles');
-  });
+    plugins.watch('src/assets/style/**/*.scss', function () {
+        gulp.start('compile-styles');
+    });
 }
 
 function compileSassStyle() {
     var sassOptions = {
         includePaths: ['lib/bower/bootstrap-sass/assets/stylesheets']
     };
-    return gulp.src('src/assets/style/index.scss')
+    var sassStream = gulp.src('src/assets/style/index.scss')
         .pipe(plugins.plumber())
         .pipe(plugins.sourcemaps.init())
             .pipe(plugins.sass(sassOptions))
             .pipe(plugins.autoprefixer('last 2 version'))
             .pipe(plugins.minifyCss())
         .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/'))
-    .pipe(plugins.connect.reload());
+        .pipe(gulp.dest('dist/'));
+
     /**
      * just tell connect server to reload css files after job is done.
      * when it get sass files on input, it reload full page,
      * css files are reload dynamically
      */
-    // sassStream.on('end', function () {
-    //     return gulp.src(gulp.destPath + '/assets/**/*.css').pipe(plugins.connect.reload());
-    // });
-    // .pipe(gulp.plugins.plumber())
-    // .pipe(plugins.sourcemaps.init())
-    // .pipe(plugins.less({
-    //   compress: true,
-    //   paths: ['src/components']
-    // }))
-    // // .pipe(plugins.concatCss('alcarin.css'))
-    // .pipe(plugins.minifyCss())
-    // // .pipe(plugins.concat('alcarin.css'))
-    // .pipe(plugins.sourcemaps.write('./'))
-    // .pipe(gulp.dest('dist/alcarin'))
-    // .pipe(plugins.connect.reload());
+    sassStream.on('end', function () {
+        return gulp.src('dist/index.css').pipe(plugins.connect.reload());
+    });
+
+    return sassStream;
 }
